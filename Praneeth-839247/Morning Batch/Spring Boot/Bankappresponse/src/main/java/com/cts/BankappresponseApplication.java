175120ce867b280eeaba28a570332838cce5d5c0
Kvpraneeth24 <Kandulavenkata.Praneeth@cognizant.com>
2020-12-11 20:07:07 +0530

    Spring Boot

---
+++ Praneeth-839247/Morning Batch/Spring Boot/Bankappresponse/src/main/java/com/cts/BankappresponseApplication.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappresponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnakappUsingEntityresponseApplication.class, args);
+	}
+
+}
